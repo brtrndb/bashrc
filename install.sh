afe52e9020e562d3476001c8411897c818f2dcae
Bertrand B <boyer.bertrand@gmail.com>
2017-11-27 17:28:47 +0100

    Added a quick installation script.

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+BASHRC=".bashrc";
+BASHRC_FOLDER=".bashrc.d"
+
+save_current () {
+    if [ -f $HOME/$BASHRC ]; then
+	echo "Saving previous $BASHRC.";
+	cp -v $HOME/$BASHRC $HOME/$BASHRC.save_`date +"%Y%m%d%H%M%S"`;
+    else
+	echo "No $BASHRC found in $HOME. Creating new one.";
+	cp -v $BASHRC $HOME/$BASHRC;
+    fi
+}
+
+copy_files () {
+    echo "Copying files from $BASHRC_FOLDER into $HOME/$BASHRC_FOLDER.";
+    cp -rv $BASHRC_FOLDER $HOME;
+}
+
+update_current () {
+    echo "Updating current $BASHRC.";
+    echo "" >> $HOME/$BASHRC;
+    echo "# Custom part." >> $HOME/$BASHRC;
+    echo ". $BASHRC.d/aliases" >> $HOME/$BASHRC;
+    echo ". $BASHRC.d/env" >> $HOME/$BASHRC;
+    echo ". $BASHRC.d/prompt" >> $HOME/$BASHRC;
+}
+
+finish () {
+    echo "You need to reload your current terminal."
+}
+
+run () {
+    save_current;
+    copy_files;
+    if [ "$1" = "-i" ]; then
+	update_current;
+    fi
+    finish;
+}
+
+run $*;

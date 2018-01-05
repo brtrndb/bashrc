8d8c642d7e7c9ac7536fd012cbfcea4f1c4467cd
Bertrand B <boyer.bertrand@gmail.com>
2018-01-05 11:25:11 +0100

    Updated install script.

--- install.sh
+++ install.sh
@@ -1,41 +1,82 @@
 #!/bin/bash
 
 BASHRC=".bashrc";
-BASHRC_FOLDER=".bashrc.d"
+BASHRC_D=".bashrc.d";
+HOME_BASHRC=$HOME/$BASHRC;
+HOME_BASHRC_D=$HOME/$BASHRC_D;
 
-save_current () {
-    if [ -f $HOME/$BASHRC ]; then
-	echo "Saving previous $BASHRC.";
-	cp -v $HOME/$BASHRC $HOME/$BASHRC.save_`date +"%Y%m%d%H%M%S"`;
-    else
-	echo "No $BASHRC found in $HOME. Creating new one.";
-	cp -v $BASHRC $HOME/$BASHRC;
+copy_files () {
+    echo "Copying files from $BASHRC_D into $HOME_BASHRC_D.";
+    cp -rv $BASHRC_D $HOME;
+}
+
+delete_files (){
+    if [ -d $HOME_BASHRC_D ]; then
+	echo "Deleting files from $HOME_BASHRC_D.";
+	rm -vrf $HOME_BASHRC_D;
     fi
 }
 
-copy_files () {
-    echo "Copying files from $BASHRC_FOLDER into $HOME/$BASHRC_FOLDER.";
-    cp -rv $BASHRC_FOLDER $HOME;
+save_current_bachrc () {
+    if [ -f $HOME_BASHRC ]; then
+	echo "Saving previous $BASHRC.";
+	cp -v $HOME_BASHRC $HOME_BASHRC.save_`date +"%Y%m%d%H%M%S"`;
+    else
+	echo "No $BASHRC found in $HOME. Creating new one.";
+	cp -v $BASHRC $HOME_BASHRC;
+    fi
 }
 
-update_current () {
+update_current_bashrc () {
     echo "Updating current $BASHRC.";
     echo "" >> $HOME/$BASHRC;
     echo "# Custom part." >> $HOME/$BASHRC;
-    echo ". $BASHRC.d/init.sh" >> $HOME/$BASHRC;
+    echo ". $HOME_BASHRC_D/init.sh" >> $HOME/$BASHRC;
 }
 
-finish () {
+restore_previous_bashrc () {
+    LAST=`find $HOME -maxdepth 1 -name $BASHRC.save* -print0  | xargs -0 ls -1 -t | head -1`;
+    echo "Most recent save is: $LAST.";
+    cp -vf $LAST $HOME_BASHRC;
+    echo "Current $HOME_BASHRC replaced by $LAST.";
+}
+
+finalize () {
     echo "You need to reload your current terminal."
 }
 
+usage () {
+    echo "Usage:";
+    echo "-n, --new:    New install."
+    echo "-c, --clean:  Rollback to previous install."
+    echo "-u, --update: Update files in $HOME_BASHRC_D.";
+    echo "-h, --help:   Display usage."
+}
+
 run () {
-    copy_files;
-    if [ "$1" = "-i" ]; then
-	save_current;
-	update_current;
-    fi
-    finish;
+    case "$1" in
+	-n | --new)
+	    copy_files;
+	    save_current_bachrc;
+	    update_current_bashrc;
+	    finalize;
+	    ;;
+	-c | --clean)
+	    delete_files;
+	    restore_previous_bashrc;
+	    finalize;
+	    ;;
+	-u | --update)
+	    copy_files;
+	    finalize;
+	    ;;
+	-h | --help)
+	    usage;
+	    ;;
+	*)
+	    usage;
+	    ;;
+    esac
 }
 
 run $*;

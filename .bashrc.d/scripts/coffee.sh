e8d73bd5ce118186b5ac301f6fdcc73a52ba8a4b
Bertrand B <boyer.bertrand@gmail.com>
2018-10-17 12:27:36 +0200

    Updated coffee.sh.

--- .bashrc.d/scripts/coffee.sh
+++ .bashrc.d/scripts/coffee.sh
@@ -1,4 +1,33 @@
 #!/bin/sh
 # Bertrand B.
 
-cat /dev/urandom | hexdump -C | grep --color=auto "ca fe";
+usage () {
+    echo "Usage: $(basename "$0") { -h }"
+    echo "-h, --help: Display usage.";
+}
+
+configure() {
+    while [ "$#" -gt "0" ]; do
+	case "$1" in
+	    -h | --help)
+		usage;
+		exit 0;
+		;;
+	    -* | --*)
+		echo "Unknown option: $1. Ignored."
+		shift 1;
+		;;
+	esac
+    done
+}
+
+coffee() {
+    cat /dev/urandom | hexdump -C | grep --color=auto "ca fe";
+}
+
+run() {
+    configure $*;
+    coffee;
+}
+
+run $*;

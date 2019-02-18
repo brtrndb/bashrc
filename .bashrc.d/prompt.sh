3821547dbb52ce8c87989e6f2c072ed4a578394e
Bertrand B <boyer.bertrand@gmail.com>
2019-02-18 13:11:38 +0100

    Full rewrite of prompt function.

--- .bashrc.d/prompt.sh
+++ .bashrc.d/prompt.sh
@@ -1,100 +1,52 @@
 # Prompt.
 
-setup_pwd() {
-    local DIR="";
-    if [[ $1 != 0 ]];
-    then
-	DIR=${PWD/$HOME/\~};
-    else
-	local GIT_ROOT=`git rev-parse --show-toplevel 2> /dev/null`;
-	local GIT_ROOT_NAME=`basename $GIT_ROOT`;
-	DIR="/${PWD/$GIT_ROOT/$GIT_ROOT_NAME}";
-    fi
-
-    if [[ $COLUMNS/4 -le ${#DIR} ]];
-    then
-	DIR="..."${DIR:${#DIR}-$COLUMNS/4:${#DIR}}
-    fi
-
-    echo -n "$DIR";
-}
-
-color_pwd() {
-    DIR="";
-    if [[ $1 != 0 ]]; then
-	DIR="${YELLOW}$2"
-    else
-	DIR="${BLUE}git${MAGENTA}:${YELLOW}$2";
-    fi
-
-    echo -n $DIR;
-}
-
-setup_git_branch() {
-    if [[ $1 != 0 ]];
-    then
-	echo -n "";
-	return 0;
-    fi
-
-    local STATUS=`git status --porcelain 2> /dev/null | wc -l`
-    local BRANCH=`git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/\1/"`;
-    local COMMIT_LOCAL=`git cherry -v origin/"$BRANCH" 2> /dev/null | wc -l`;
-    local COMMIT_REMOTE=`git log --oneline HEAD..origin/"$BRANCH" 2> /dev/null | wc -l`;
-
-    if [[ $STATUS != 0 ]];
-    then
-	STATUS_COLOR="${RED}"
-    else
-	STATUS_COLOR="${RESET}"
-    fi
-
-    local GIT_COMMIT="";
-    if [[ $COMMIT_LOCAL != 0 ]];
-    then
-	GIT_COMMIT="$GREEN$COMMIT_LOCAL↑";
-    fi
-    if [[ $COMMIT_REMOTE != 0 ]];
-    then
-	GIT_COMMIT="$GIT_COMMIT${YELLOW}$COMMIT_REMOTE↓";
-    fi
-
-    local GIT_BRANCH="${MAGENTA}[${RESET}$STATUS_COLOR$BRANCH";
-    if [ -n "$GIT_COMMIT" ]
-    then
-	GIT_BRANCH="$GIT_BRANCH${MAGENTA}|"
-    fi
-
-    local GIT="$GIT_BRANCH$GIT_COMMIT${MAGENTA}]${RESET}";
-
-    echo -n " $GIT";
-}
-
 my_prompt() {
-    local EXIT_CODE="$?"
+  local LAST_CMD_EXIT_CODE="$?";
+  local PS_USER_COLOR=$([ "$LAST_CMD_EXIT_CODE" = "0" ] && echo "$CYAN" || echo "$RED");
 
-    local COLOR=""
-    if [ $EXIT_CODE = 0 ];
-    then
-	COLOR="${CYAN}";
-    else
-	COLOR="${RED}";
+  git rev-parse --git-dir > /dev/null 2>&1;
+  local IS_WD_GIT_REPO="$?";
+
+  local GIT_INFOS;
+  local CURRENT_DIR;
+  if [ "$IS_WD_GIT_REPO" != "0" ]; then
+    CURRENT_DIR=${PWD/$HOME/\~};
+  else
+    local GIT_ROOT=$(git rev-parse --show-toplevel 2> /dev/null);
+    CURRENT_DIR="/${PWD/$GIT_ROOT/$(basename "$GIT_ROOT")}";
+
+    local GIT_NB_FILES=$(git status --porcelain 2> /dev/null | wc -l);
+    local GIT_BRANCH_NAME=$(git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/\1/");
+    local GIT_NB_COMMIT_LOCAL=$(git cherry -v origin/"$BRANCH" 2> /dev/null | wc -l);
+    local GIT_NB_COMMIT_REMOTE=$(git log --oneline HEAD..origin/"$BRANCH" 2> /dev/null | wc -l);
+
+    local GIT_STATUS;
+    if [[ "$GIT_NB_FILES" != "0" ]]; then
+      GIT_STATUS="$RED±$GIT_NB_FILES$RESET";
+    fi
+    if [[ "$GIT_NB_COMMIT_LOCAL" != "0" ]]; then
+      GIT_STATUS="$GIT_STATUS$GREEN↑$GIT_NB_COMMIT_LOCAL$RESET";
+    fi
+    if [[ "$GIT_NB_COMMIT_REMOTE" != "0" ]]; then
+      GIT_STATUS="$GIT_STATUS$YELLOW↓$GIT_NB_COMMIT_REMOTE$RESET";
     fi
 
-    local CHROOT="${debian_chroot:+($debian_chroot)|}";
-    local TIME="\A${MAGENTA}|${RESET}";
-    local USER="${COLOR}\u${MAGENTA}@${COLOR}\H${RESET}";
-    IS_GIT=`git status 2> /dev/null`;
-    local IS_GIT_CODE="$?";
+    GIT_INFOS="${MAGENTA}[$RESET$BOLD$GIT_BRANCH_NAME$DEFAULT$MAGENTA|$GIT_STATUS$MAGENTA]$RESET";
+  fi
 
-    NEW_PWD=`setup_pwd $IS_GIT_CODE`;
-    COLOR_PWD=`color_pwd $IS_GIT_CODE $NEW_PWD`;
-    local TITLEBAR='\033]0;${NEW_PWD}\007';
-    local DIR="${MAGENTA}:${COLOR_PWD}${RESET}";
-    local GIT=`setup_git_branch $IS_GIT_CODE`;
-    local SU="\$ > ";
+  if [[ $COLUMNS/4 -le ${#CURRENT_DIR} ]]; then
+    CURRENT_DIR="..."${CURRENT_DIR:${#CURRENT_DIR}-$COLUMNS/4:${#CURRENT_DIR}}
+  fi
 
-    PS1="$TITLEBAR$CHROOT$TIME$USER$DIR$GIT$SU";
+  local PS_TITLEBAR="\\033]0;$CURRENT_DIR\\007";
+  local PS_CHROOT="${debian_chroot:+($debian_chroot)|}";
+  local PS_TIME="\\A$MAGENTA|$RESET";
+  local PS_USER="$PS_USER_COLOR\\u$MAGENTA@$PS_USER_COLOR\\H$MAGENTA:$RESET";
+  local PS_CURRENT_DIR=$([ "$IS_WD_GIT_REPO" != "0" ] && echo "$YELLOW$CURRENT_DIR$RESET" || echo "${BLUE}git$MAGENTA:$YELLOW$CURRENT_DIR");
+  local PS_GIT=$([ "$IS_WD_GIT_REPO" = "0" ] && echo " $GIT_INFOS" || echo "");
+  local PS_SU="\$ > ";
+
+  PS1="$PS_TITLEBAR$PS_CHROOT$PS_TIME$PS_USER$PS_CURRENT_DIR$PS_GIT$PS_SU";
 }
 
 # Export prompt.

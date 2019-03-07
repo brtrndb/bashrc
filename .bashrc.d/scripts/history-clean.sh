c99ebc78d60d64d014420d13577797b059f292be
Bertrand B <boyer.bertrand@gmail.com>
2019-03-07 12:13:30 +0100

    Rewritten history-clean script.

--- .bashrc.d/scripts/history-clean.sh
+++ .bashrc.d/scripts/history-clean.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
 # Bertrand B.
 
-rm -f $HOME/.bash_history && clear && history -c;
+usage() {
+  echo "Empty your command line history and delete your .bash_history.";
+  echo "";
+  echo "Usage: $(basename "$0") [OPTIONS]";
+  echo "  -h, --help: Display usage.";
+}
+
+history_clean() {
+  rm -f "$HOME/.bash_history" && clear && history -c;
+}
+
+run() {
+  if [ $# -ge 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    usage;
+    return;
+  fi
+
+  history_clean;
+}
+
+run "$*";

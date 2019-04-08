86cf37535841481988acee50dc51db31e360fb4a
Bertrand B <boyer.bertrand@gmail.com>
2019-04-08 13:27:54 +0200

    Fix completion script.

--- .bashrc.d/completion.sh
+++ .bashrc.d/completion.sh
@@ -1,5 +1,3 @@
-if ! shopt -oq posix; then
-  if [ -f ./yarn-completion/yarn-completion.bash ]; then
-    . ./yarn-completion/yarn-completion.bash
-  fi
+if [ -f ./yarn-completion/yarn-completion.bash ]; then
+  . ./yarn-completion/yarn-completion.bash
 fi
